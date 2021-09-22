47a6f6430e10bcc3b0b9ce6965d13b3aeec06b35
Curtis Castro <curtisncastro@gmail.com>
2021-09-22 13:01:29 -0700

    cleanup imports and dependencies in CValid.js

--- src/views/Components/CValid.js
+++ src/views/Components/CValid.js
@@ -1,64 +1,28 @@
-import React from "react";
+import React from 'react';
 // nodejs library that concatenates classes
-import classNames from "classnames";
+// import classNames from 'classnames';
 // react components for routing our app without refresh
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import styled from 'styled-components';
+import { makeStyles } from '@material-ui/core/styles';
 // @material-ui/icons
 // core components
-import Header from "components/Header/Header.js";
-import CurtisHeader from "components/Header/CurtisHeader.js";
-import Footer from "components/Footer/Footer.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import CustomTabs from "components/CustomTabs/CustomTabs.js";
-import Button from "components/CustomButtons/Button.js";
-import Parallax from "components/Parallax/Parallax.js";
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import GridContainer from '../../components/Grid/GridContainer';
+import GridItem from '../../components/Grid/GridItem';
+import CustomTabs from '../../components/CustomTabs/CustomTabs';
 
-// import CanvasJSReact from "/Users/curtiscastro/work/Projects/portfolio-2021/src/components/canvasjs-3.4.1/canvasjs.react.js";
-
-// @material-ui/icons
-import Face from "@material-ui/icons/Face";
-import Chat from "@material-ui/icons/Chat";
-import Build from "@material-ui/icons/Build";
-import LocationOn from "@material-ui/icons/LocationOn";
-import Speed from "@material-ui/icons/Speed";
-import LocalOffer from "@material-ui/icons/LocalOffer";
-import Subscriptions from "@material-ui/icons/Subscriptions";
-import Camera from "@material-ui/icons/Camera";
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-
-
-// Curtis' custom components
-
-
-
+// styles
+import styles from '../../assets/jss/material-kit-react/views/components';
 
 // sections for this page
-import CurtisHeaderLinks from "components/Header/CurtisHeaderLinks.js";
-import CanvasJSReact from "/Users/curtiscastro/work/Projects/portfolio-2021/src/components/canvasjs-3.4.1/canvasjs.react.js"
-import LineChart from "/Users/curtiscastro/work/Projects/portfolio-2021/src/components/LineChart/LineChart.js";
-import SectionCarousel from "views/Components/Sections/SectionCarousel.js";
-import XSectionToolbar from "views/Components/Sections/XSectionToolbar.js";
-import XSectionVideoPlayer from "views/Components/Sections/XSectionVideoPlayer.js";
-
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-
-import styles from "assets/jss/material-kit-react/views/components.js";
-
-import styled from "styled-components";
-
+import XSectionToolbar from './Sections/XSectionToolbar';
+import XSectionVideoPlayer from './Sections/XSectionVideoPlayer';
+import CurtisHeaderLinks from '../../components/Header/CurtisHeaderLinks';
 
 const useStyles = makeStyles(styles);
-
-const CenteredContent = styled.div`
-  margin: 0 auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
 
 const Spacer = styled.div`
   height: 20px;
@@ -67,40 +31,6 @@ const Spacer = styled.div`
 const UnderHeaderSpacer = styled.div`
   height: 70px;
   border: 3px dotted red;
-`;
-
-const ExtCam = styled.iframe`
-  margin: 0px auto;
-  width: 100%;
-  aspect-ratio: 16 / 9;
-`;
-
-const IntCam = styled.iframe`
-  width: 70%;
-  aspect-ratio: 16 / 9;
-`;
-
-const MainVideoDisplay = styled.div`
-  color: grey;
-  background: white;
-  border-radius: 5px;
-  width: 100%;
-  padding: 15px;
-  box-shadow: 1px 2px 1px grey;
-  min-height: 440px;
-`;
-
-const TitleText = styled.h3`
-  margin: 0;
-  font-size: .8em;
-  line-height: 1em;
-  padding-bottom: 4px;
-`;
-
-const TitleMetaData = styled.p`
-  margin: 0;
-  font-size: .8em;
-  line-height: 1em;
 `;
 
 const ActionItemBoxStatic = styled.div`
@@ -123,7 +53,6 @@ export default function Components(props) {
   const { ...rest } = props;
   return (
     <div className={classes.section}>
-
       <Header
         brand="Curtis de Castro"
         rightLinks={<CurtisHeaderLinks />}
@@ -131,7 +60,7 @@ export default function Components(props) {
         color="danger"
         changeColorOnScroll={{
           height: 400,
-          color: "transparent",
+          color: 'transparent',
         }}
         {...rest}
       />
@@ -139,41 +68,41 @@ export default function Components(props) {
 
       <div className={classes.container}>
 
-          <GridContainer>
-            <GridItem xs={12} sm={12} md={7}>
-              <Spacer/>
-              <XSectionVideoPlayer />
-            </GridItem>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={7}>
+            <Spacer />
+            <XSectionVideoPlayer />
+          </GridItem>
 
-            <GridItem xs={12} sm={12} md={5}>
-              <XSectionToolbar/>
-            </GridItem>
-          </GridContainer>
-          <CustomTabs
-            plainTabs
-            headerColor="danger"
-            tabs={[
-              {
-                tabName: "Tags",
-                tabContent: (
-                  <ActionItemBoxStatic/>
-                ),
-              },
-              {
-                tabName: "Comments",
-                tabContent: (
-                  <ActionItemBoxVariable/>
-                ),
-              },
-              {
-                tabName: "Status",
-                tabContent: (
-                  <ActionItemBoxStatic/>
-                ),
-              },
-            ]}
-          />
-          <Footer />
+          <GridItem xs={12} sm={12} md={5}>
+            <XSectionToolbar />
+          </GridItem>
+        </GridContainer>
+        <CustomTabs
+          plainTabs
+          headerColor="danger"
+          tabs={[
+            {
+              tabName: 'Tags',
+              tabContent: (
+                <ActionItemBoxStatic />
+              ),
+            },
+            {
+              tabName: 'Comments',
+              tabContent: (
+                <ActionItemBoxVariable />
+              ),
+            },
+            {
+              tabName: 'Status',
+              tabContent: (
+                <ActionItemBoxStatic />
+              ),
+            },
+          ]}
+        />
+        <Footer />
 
       </div>
     </div>
--- src/views/Components/Components.js
+++ src/views/Components/Components.js
@@ -1,34 +1,41 @@
-// @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
-import styles from "assets/jss/material-kit-react/views/components.js";
-// nodejs library that concatenates classes
-import classNames from "classnames";
-import Button from "components/CustomButtons/Button.js";
-import Footer from "components/Footer/Footer.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-// @material-ui/icons
-// core components
-import Header from "components/Header/Header.js";
-// sections for this page
-import HeaderLinks from "components/Header/HeaderLinks.js";
-import Parallax from "components/Parallax/Parallax.js";
-import React from "react";
-// react components for routing our app without refresh
-import { Link } from "react-router-dom";
-import SectionBasics from "./Sections/SectionBasics.js";
-import SectionCarousel from "./Sections/SectionCarousel.js";
-import SectionCompletedExamples from "./Sections/SectionCompletedExamples.js";
-import SectionDownload from "./Sections/SectionDownload.js";
-import SectionExamples from "./Sections/SectionExamples.js";
-import SectionJavascript from "./Sections/SectionJavascript.js";
-import SectionLogin from "./Sections/SectionLogin.js";
-import SectionNavbars from "./Sections/SectionNavbars.js";
-import SectionNotifications from "./Sections/SectionNotifications.js";
-import SectionPills from "./Sections/SectionPills.js";
-import SectionTabs from "./Sections/SectionTabs.js";
-import SectionTypography from "./Sections/SectionTypography.js";
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+// @material-ui/core components
+import { makeStyles } from '@material-ui/core/styles';
+
+// nodejs library that concatenates classes
+import classNames from 'classnames';
+
+// core components
+import Header from '../../components/Header/Header';
+import HeaderLinks from '../../components/Header/HeaderLinks';
+import Parallax from '../../components/Parallax/Parallax';
+
+import GridContainer from '../../components/Grid/GridContainer';
+import GridItem from '../../components/Grid/GridItem';
+import Footer from '../../components/Footer/Footer';
+import Button from '../../components/CustomButtons/Button';
+
+// @material-ui/icons
+
+// react components for routing our app without refresh
+
+import SectionBasics from './Sections/SectionBasics';
+import SectionCarousel from './Sections/SectionCarousel';
+import SectionCompletedExamples from './Sections/SectionCompletedExamples';
+import SectionDownload from './Sections/SectionDownload';
+import SectionExamples from './Sections/SectionExamples';
+import SectionJavascript from './Sections/SectionJavascript';
+import SectionLogin from './Sections/SectionLogin';
+import SectionNavbars from './Sections/SectionNavbars';
+import SectionNotifications from './Sections/SectionNotifications';
+import SectionPills from './Sections/SectionPills';
+import SectionTabs from './Sections/SectionTabs';
+import SectionTypography from './Sections/SectionTypography';
+
+import styles from '../../assets/jss/material-kit-react/views/components';
 
 const useStyles = makeStyles(styles);
 
@@ -44,11 +51,11 @@ export default function Components(props) {
         color="transparent"
         changeColorOnScroll={{
           height: 400,
-          color: "white",
+          color: 'white',
         }}
         {...rest}
       />
-      <Parallax image={require("assets/img/bg7.jpg").default}>
+      <Parallax image={require('../../assets/img/bg7.jpg').default}>
         <div className={classes.container}>
           <GridContainer>
             <GridItem>
@@ -59,7 +66,6 @@ export default function Components(props) {
           </GridContainer>
         </div>
       </Parallax>
-
       <div className={classNames(classes.main, classes.mainRaised)}>
         <SectionBasics />
         <SectionNavbars />
@@ -72,7 +78,7 @@ export default function Components(props) {
         <SectionCompletedExamples />
         <SectionLogin />
         <GridItem md={12} className={classes.textCenter}>
-          <Link to={"/login-page"} className={classes.link}>
+          <Link to="login-page" className={classes.link}>
             <Button color="primary" size="lg" simple>
               View Login Page
             </Button>
